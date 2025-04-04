767c845acaeb60a701a899fd46047efd70dbc1de
Meenakshi1011 <meenakshi.j.1003@gmail.com>
2025-04-04 12:57:19 +0530

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import Companies from "./components/companies/companies";
+import Companies from "./components/Companies/Companies";
 import Residency from "./components/Residency/Residency";
 import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
